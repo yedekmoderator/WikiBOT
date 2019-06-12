a57eafbcd6b3b6344f2935571f03c5d80393a4e6
iWikipedia <unconfigured@null.spigotmc.org>
2019-06-12 11:43:12 -0500

    making embed work and polishing stuff

--- data/commands/embed.js
+++ data/commands/embed.js
@@ -9,10 +9,10 @@ exports.run = (client, message, args) => {
             .setColor(client.color.red)
             .setAuthor("❌ No title!")
             .setDescription("No title was provided!")
-            .addField("Correct Usage:", `${client.prefix}embed <title> <message>`)
+            .addField("Correct Usage:", "**``" + client.prefix + "embed <title> <message>``**")
             .setTimestamp()
             .setFooter(`Command issued by ${message.author.tag}`, message.author.avatarURL)
-        return message.channel.send(notitle)
+        return message.channel.send(notitle).then(m => { m.delete(3000); message.delete(3000) })
     } 
 
     if(!message1) {
@@ -20,9 +20,17 @@ exports.run = (client, message, args) => {
             .setColor(client.color.red)
             .setAuthor("❌ No message!")
             .setDescription("No message was provided!")
-            .addField("Correct Usage:", `${client.prefix}embed <title> <message>`)
+            .addField("Correct Usage:", "**``" + client.prefix + "embed <title> <message>``**")
             .setTimestamp()
             .setFooter(`Command issued by ${message.author.tag}`, message.author.avatarURL)
-        return message.channel.send(notitle)
-    } 
+        return message.channel.send(notitle).then(m => { m.delete(3000); message.delete(3000) })
+    }
+
+    const embedmsg = new Discord.RichEmbed()
+        .setColor(client.color.blue)
+        .setAuthor(title)
+        .setDescription(message1)
+        .setTimestamp()
+        .setFooter(`Command issued by ${message.author.tag}`, message.author.avatarURL)
+    message.channel.send(embedmsg)
 }
